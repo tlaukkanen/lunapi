28c96eb5a7833b80b258a3c743647330ed66ca4c
Tommi Laukkanen <tlaukkanen@gmail.com>
2020-11-30 17:16:38 +0000

    testing

--- src/face.py
+++ src/face.py
@@ -7,7 +7,7 @@ import adafruit_ssd1306
 
 #def rounded_rectangle(self: ImageDraw, xy, corner_radius, fill=None, outline=None):
 
-def drawRoundRec(self: ImageDraw, color, x, y, w, h, r):    
+def rounded_rectangle(self: ImageDraw, color, x, y, w, h, r):    
   '''Rounds'''    
   self.ellipse((x,y,x+r,y+r),fill=color)    
   self.ellipse((x+w-r,y,x+w,y+r),fill=color)    
@@ -28,25 +28,26 @@ class Face:
 
     self.i2c = board.I2C()
     self.oled = adafruit_ssd1306.SSD1306_I2C(128, 32, self.i2c, addr=0x3C, reset=RESET_PIN)
-    #oled.rotation = 2
-    
+    self.oled.rotation = 1
+    #self.oled.set_contrast(64)
+
     # Clear display
     self.oled.fill(0)
     self.oled.show()
 
-    self.width = self.oled.width
-    self.height = self.oled.height
+    self.width = 32 #self.oled.width
+    self.height = 128 #self.oled.height
 
     # Create blank image for drawing.
-    self.image = Image.new("1", (self.oled.width, self.oled.height))
+    self.image = Image.new("1", (32, 128))
     self.draw = ImageDraw.Draw(self.image)
-
+    self.draw.rounded_rectangle = rounded_rectangle
     # Load a font in 2 different sizes.
     self.font = ImageFont.truetype("../fonts/EarlyGameBoy.ttf", 16)
 
   def drawEyes(self):
-    x = random.randint(0, 100)
-    y = random.randint(0, 4)
+    x = random.randint(0, 12)
+    y = random.randint(20, 64)
 
     # Draw a black filled box to clear the image.
     self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
@@ -54,9 +55,11 @@ class Face:
 
     #self.draw.ellipse((x,y,x+28,y+28), outline=0, fill=255)
     #self.draw.ellipse((x+10,y+10,x+18,y+18), outline=0, fill=0)
-    self.draw.rounded_rectangle(255, x,y,28,28,8)
+    self.draw.rounded_rectangle(self.draw, 255, x, y, 20, 40, 20)
+    self.draw.rectangle((x, y+30, x+10, y+40), outline=0, fill=0)
     
     # draw.text((0, 0), "Robo R01", font=font, fill=255)
+    
     self.oled.image(self.image)
     self.oled.show()
 
--- src/tracktor.py
+++ src/tracktor.py
@@ -1,32 +1,35 @@
 import time
-#from adafruit_servokit import ServoKit
+from adafruit_servokit import ServoKit
 
 from adafruit_motorkit import MotorKit
 
 motors = MotorKit(0x40)
 
-#kit = ServoKit(channels=16)
+kit = ServoKit(channels=16)
 
 #kit.servo[0].actuation_range = 200
-#kit.servo[0].angle = 100
+kit.servo[0].angle = 90
 
 time.sleep(1)
 
-motors.motor2.throttle =  0.50
-motors.motor1.throttle = -0.50
+motors.motor2.throttle = -0.80
+motors.motor1.throttle = 0.80
 
 time.sleep(2)
 
 motors.motor2.throttle = 0
 motors.motor1.throttle = 0
+
 #kit.servo[0].angle = 100
 time.sleep(1)
 #time.sleep(1)
-motors.motor2.throttle = -1.0
-motors.motor1.throttle =  1.0
+motors.motor2.throttle = 0.50
+motors.motor1.throttle = 0.50
 #kit.servo[0].angle = 70
-#time.sleep(1)
+time.sleep(1)
 #kit.servo[0].angle = 100
-time.sleep(2)
+#time.sleep(2)
 motors.motor2.throttle = 0
 motors.motor1.throttle = 0
+
+kit.servo[0].angle = 70
